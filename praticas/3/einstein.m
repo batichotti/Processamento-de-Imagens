550c0e0cc345de73cb64a52fa80cf50706c5bde3
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2025-09-15 13:33:44 -0300

    Implement histogram equalization for image
    Added code to perform histogram equalization on 'imagem.jpg' and save the result as 'imagem_saida.jpg'. The script now loads the image, computes the transformation, applies it, and plots the histogram of the output image.

--- praticas/3/einstein.m
+++ praticas/3/einstein.m
@@ -1,4 +1,28 @@
-L = 256;
-img = imread('imagem.jpg');
+pkg load image;
 
-imshow(img);
+img = imread('imagem.jpg');
+[img_M, img_N] = size(img);
+img_MN = img_M*img_N;
+
+[counts, x] = imhist(img);
+L = length(counts);
+
+T = zeros(1, L);
+
+for i = 1:L
+  T(i) = round(((L-1)/img_MN)*sum(counts(1:i)));
+  endfor;
+
+img_output = uint8(zeros(img_M, img_N));
+
+for i = 1:img_M
+  for j = 1:img_N
+    img_output(i, j) = T(img(i, j));
+    endfor;
+  endfor;
+
+[countsf, xf] = imhist(img_output);
+
+imwrite(img_output, 'imagem_saida.jpg');
+
+plot(xf, countsf);
